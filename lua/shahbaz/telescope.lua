32b1f23a934ce34b94b0fe964c6f013474b58e23
Shay Siddiqui <ssiddiqui@connected.io>
2022-07-20 20:11:12 +0100

    location list

--- lua/shahbaz/telescope.lua
+++ lua/shahbaz/telescope.lua
@@ -43,9 +43,12 @@ telescope.setup({
 
 				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
 				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-				["<C-l>"] = actions.complete_tag,
+				-- ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				-- ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				-- ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-l>"] = actions.send_selected_to_loclist + actions.open_loclist,
+				["<C-a>"] = actions.send_to_loclist + actions.open_loclist,
+				-- ["<C-l>"] = actions.complete_tag,
 				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
 			},
 
@@ -58,7 +61,9 @@ telescope.setup({
 
 				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
 				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				-- ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<C-l>"] = actions.send_selected_to_loclist + actions.open_loclist,
+				["<C-a>"] = actions.send_to_loclist + actions.open_loclist,
 				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 
 				["j"] = actions.move_selection_next,
